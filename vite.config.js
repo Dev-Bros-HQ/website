0f436e11f2ef004360d12ecf365d38a2c472a8ed
Richard Hancock <richardl@hancock1.com>
2023-01-02 23:07:12 -0700

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import ssr from "vite-plugin-ssr/plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ssr({ prerender: true })],
+  build: {
+    target: "esnext",
+  },
 });
