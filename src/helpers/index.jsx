d893ede9cd06569179349a622c38a56034fba762
Rico Hancock <richardl@hancock1.com>
2023-03-06 17:49:41 -0700

    website/daily-todo

--- src/helpers/index.jsx
+++ src/helpers/index.jsx
@@ -26,3 +26,15 @@ export const isValidEmail = (string) => {
     string
   );
 };
+
+export const getReadableTime = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+
+  return formattedTime;
+};
--- src/renderer/Footer.jsx
+++ src/renderer/Footer.jsx
@@ -1,14 +1,14 @@
 import logo from "../assets/circle-dev-bros-hq.webp";
 const Footer = () => {
   return (
-    <footer className="bg-secondary-focus text-neutral-content body-font py-2">
-      <div className="card bg-base-300 container px-5 py-12 mx-auto">
-        <div className="flex flex-wrap md:text-left order-first">
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-neutral-content tracking-widest text-sm mb-3">
+    <footer className="body-font bg-secondary-focus py-2 text-neutral-content">
+      <div className="container card mx-auto bg-base-300 px-5 py-12">
+        <div className="order-first flex flex-wrap md:text-left">
+          <div className="w-full px-4 md:w-1/2 lg:w-1/4">
+            <h2 className="title-font mb-3 text-sm font-medium tracking-widest text-neutral-content">
               Links
             </h2>
-            <nav className="list-none mb-10">
+            <nav className="mb-10 list-none">
               <ul>
                 <li>
                   <a href="/" className="text-base">
@@ -33,11 +33,11 @@ const Footer = () => {
               </ul>
             </nav>
           </div>
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium tracking-widest text-sm mb-3">
+          <div className="w-full px-4 md:w-1/2 lg:w-1/4">
+            <h2 className="title-font mb-3 text-sm font-medium tracking-widest">
               Tools
             </h2>
-            <nav className="list-none mb-10">
+            <nav className="mb-10 list-none">
               <ul>
                 <li>
                   <a href="/mw2-builds" className="text-base">
@@ -54,25 +54,25 @@ const Footer = () => {
                     Box Breathing
                   </a>
                 </li>
-                {/* <li>
-                  <a href="/" className="text-base">
-                    Fourth Link
+                <li>
+                  <a href="/daily-todo" className="text-base">
+                    Daily Todos
                   </a>
-                </li> */}
+                </li>
               </ul>
             </nav>
           </div>
         </div>
         <div>
-          <div className="container px-4 pt-2 pb-0 mx-auto flex items-start sm:items-center sm:flex-row flex-col">
+          <div className="container mx-auto flex flex-col items-start px-4 pt-2 pb-0 sm:flex-row sm:items-center">
             <a
               href="/"
-              className="flex title-font font-medium items-center md:justify-start justify-center text-neutral-content"
+              className="title-font flex items-center justify-center font-medium text-neutral-content md:justify-start"
             >
-              <img src={logo} alt="dev bros hq logo" className="w-10 h-10" />
+              <img src={logo} alt="dev bros hq logo" className="h-10 w-10" />
               <span className="ml-3 text-xl">Dev Bros HQ</span>
             </a>
-            <p className="text-sm sm:ml-6 sm:mt-0 mt-4">© 2022 Dev Bros HQ</p>
+            <p className="mt-4 text-sm sm:ml-6 sm:mt-0">© 2022 Dev Bros HQ</p>
           </div>
         </div>
       </div>
